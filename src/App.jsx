73e5feba5b1d20ba862be9d5b16e748bcf888b5d
Z-4r <raunakper777@gmail.com>
2025-04-04 02:09:06 +0530

    Added static page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='container'>
       <h1>Expense Tracker</h1>
+        <div className='expense-form'>
+          <input type='text' placeholder='Expense Description' />
+          <input type='number' placeholder='Amount' />
+          <button>Add Expense</button>
+        </div>
+        <ul className='expense-list'>
+          <li> Food - ₹500</li>
+          <li> Transport - ₹200</li>
+        </ul>
     </div>
-  )
+  );
 }
 
 export default App
