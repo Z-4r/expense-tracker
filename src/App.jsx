b873b818ef50c2380129351c66ce47d495c94e8a
Z-4r <raunakper777@gmail.com>
2025-04-04 03:14:42 +0530

    added delete button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import "./index.css";
 
-const App = () => {
+function App() {
+  const [expenses, setExpenses] = useState([]);
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  
+  const addExpense = () => {
+    if(description.trim() === "" || amount.trim() === "" || isNaN(parseFloat(amount))){
+      alert("Please Enter a valid description and amount!");
+      return;
+  }
+
+  const newExpense = {
+    id: Date.now(),
+    description,
+    amount: parseFloat(amount),
+  };
+
+  setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+  setDescription("");
+  setAmount("");
+};
+
+const deleteExpense = (id) => {
+  setExpenses(expenses.filter((expense)=> expense.id !== id));
+};
+
   return (
     <div className='container'>
       <h1>Expense Tracker</h1>
         <div className='expense-form'>
-          <input type='text' placeholder='Expense Description' />
-          <input type='number' placeholder='Amount' />
-          <button>Add Expense</button>
+          <input type='text' placeholder='Expense Description' value={description} onChange={(e)=> setDescription(e.target.value)}/>
+          <input type='number' placeholder='Amount' value={amount} onChange={(e)=> setAmount(e.target.value)}/>
+          <button onClick={addExpense}>Add Expense</button>
         </div>
         <ul className='expense-list'>
-          <li> Food - ₹500</li>
-          <li> Transport - ₹200</li>
+          {expenses.map((expense) => (
+            <li key={expense.id}>
+              {expense.description} - ₹{expense.amount}
+              <button className='delete-btn' onClick={()=>deleteExpense(expense.id)}>Delete</button>
+            </li>
+          ))}
         </ul>
+        <h2>Total: ₹{expenses.reduce((total, expense) => total + expense.amount, 0)}</h2>
     </div>
   );
 }
